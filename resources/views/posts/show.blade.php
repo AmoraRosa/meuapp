83c6938f028d415df92477b58696861097e5e264
Amora-PC\Amora <amorarosaa@hotmail.com>
2017-07-27 15:04:17 -0300

    no message

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <a href="/post" class="btn btn-default">Voltar</a>
+    <h1>{{$post->title}}</h1>
+    <div>
+      {{$post->body}}
+    </div>
+    <hr>
+    <small>Escrito em {{$post->created_at}}</small>
+@endsection
