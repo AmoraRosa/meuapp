cad25f51d935313fb1b09325280f475e2ce239d5
Amora-PC\Amora <amorarosaa@hotmail.com>
2017-07-28 10:47:49 -0300

    vid 8

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -68,7 +68,8 @@ class PostController extends Controller
      */
     public function edit($id)
     {
-        //
+          $post = Post::find($id);
+          return view('posts.edit')->with('post', $post);
     }
 
     /**
@@ -80,7 +81,16 @@ class PostController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+      $this->validate($request,[
+        'title' => 'required',
+        'body' => 'required'
+      ]);
+      //Create Post
+      $post = Post::find($id);
+      $post->title = $request->input('title');
+      $post->body = $request->input('body');
+      $post->save();
+      return redirect('/post')->with('success', 'Post Atualizado');
     }
 
     /**
@@ -91,6 +101,8 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        $post->delete();
+        return redirect('/post')->with('success', 'Post Deletado');
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,4 +8,10 @@
     </div>
     <hr>
     <small>Escrito em {{$post->created_at}}</small>
+    <hr>
+    <a href="/post/{{$post->id}}/edit" class="btn btn-default">Editar</a>
+    {!!Form::open(['action' => ['PostController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+      {{Form::hidden('_method','DELETE')}}
+      {{Form::submit('Deletar', ['class' => 'btn btn-danger'])}}
+    {!!Form::close()!!}
 @endsection
